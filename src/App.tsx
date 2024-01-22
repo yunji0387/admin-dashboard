20b2a90d97eed05f7aaafb102b8ee63e1d798692
yunji0387 <yunji0387@gmail.com>
2024-01-22 16:38:53 -0600

    Add new pages to App.tsx and export Home component in index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import { dataProvider, liveProvider, authProvider } from "./provider";
-import { Home } from "./pages;
+import { Home, ForgotPassword, Login, Register } from "./pages";
 
 import routerBindings, {
   DocumentTitleHandler,
@@ -39,6 +39,9 @@ function App() {
                 <Routes>
                   <Route index element={<WelcomePage />} />
                   <Route index element={<Home />} />
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/forgot-password" element={<ForgotPassword />} />
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
