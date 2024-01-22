43c6c9ff9a6cd76a9b3749d97b60e70621a88ff9
yunji0387 <yunji0387@gmail.com>
2024-01-22 16:19:45 -0600

    Add authProvider and Home page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import { dataProvider, liveProvider } from "./provider";
+import { dataProvider, liveProvider, authProvider } from "./provider";
+import { Home } from "./pages;
+
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
@@ -25,7 +27,7 @@ function App() {
                 liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
-                // authProvider={authProvider}
+                authProvider={authProvider}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
@@ -36,6 +38,7 @@ function App() {
               >
                 <Routes>
                   <Route index element={<WelcomePage />} />
+                  <Route index element={<Home />} />
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
